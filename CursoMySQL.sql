8df7faad69582c8c3f2ea513d2b607b73e02cdc0
Leonardo <leonardosarto1@hotmail.com>
2022-07-10 17:30:19 -0300

    Atividade aula PK/FK

--- CursoMySQL.sql
+++ CursoMySQL.sql
@@ -18,3 +18,19 @@ INSERT INTO estado(nome, sigla, data_cadastro) VALUES('PARANÁ', 'PR', '2022-07-
 INSERT INTO estado(nome, sigla, data_cadastro) VALUES('SÃO PAULO', 'SP', '2022-07-10');
 
 SELECT id, nome, sigla, ativo, data_cadastro FROM estado;
+
+CREATE TABLE cidade(
+	id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    ativo CHAR(1) NOT NULL DEFAULT 'S',
+    data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    estado_id INT NOT NULL,
+    CONSTRAINT pk_cidade PRIMARY KEY (id),
+    CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES estado (id),
+    CONSTRAINT cidade_ativo_deve_ser_S_ou_N CHECK (ativo IN ('S', 'N')),
+    CONSTRAINT cidade_unica UNIQUE(nome, estado_id)
+);
+
+INSERT INTO cidade (nome, estado_id) VALUES('CURITIBA', 1);
+
+SELECT * FROM cidade;
